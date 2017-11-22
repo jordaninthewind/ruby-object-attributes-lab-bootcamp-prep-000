1831afaac11245e4da2a06005679c8dc49d06ffc
jordaninthewind <jordan.kline@gmail.com>
2017-11-22 21:11:11 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,15 +3,15 @@ class Person
     @name
   end
 
-  def name=(name)
-    @name = name
+  def name=(new_name)
+    @name = new_name
   end
 
   def job(job)
-    @job = job
+    @job
   end
 
-  def job=(job)
-    @job = job
+  def job=(new_job)
+    @job = new_job
   end
 end
