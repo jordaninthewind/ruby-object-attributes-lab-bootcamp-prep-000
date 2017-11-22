bda8165da1dbfa804a00cea6b8593528a10575e3
jordaninthewind <jordan.kline@gmail.com>
2017-11-22 21:19:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog
-  def name=(name)
-    @name = name
+  def name
+    @name
   end
 
-  def breed=(breed)
-    @breed = breed
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
   end
 end
